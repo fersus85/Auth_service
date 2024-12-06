e4675e1eaeb9bf745a37c843dc9f02e61242c4d7
deriabin_85 <deriabin_85@mail.ru>
2024-12-06 20:03:30 +0300

    Настройки PSQL

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,7 @@
 import os
 
+from pydantic import PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -10,8 +12,31 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "AuthService"
     BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str = ""
+
+    ECHO: bool = False
+    ECHO_POOL: bool = False
+    POOL_SIZE: int = 20
+    MAX_OVERFLOW: int = 10
+
     REDIS_HOST: str = "127.0.0.1"
     REDIS_PORT: int = 6379
+
+    @computed_field
+    @property
+    def DB_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
 
 
 settings = Settings()
