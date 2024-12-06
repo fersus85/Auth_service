480682eae19e3b68879e1a9d6b3ddd78862e6f3e
deriabin_85 <deriabin_85@mail.ru>
2024-12-06 20:19:07 +0300

    Проверочный эндпоинт

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -1,6 +1,10 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.postrges_db.psql import get_db
 
 logger = logging.getLogger(__name__)
 
@@ -8,5 +12,10 @@ router = APIRouter()
 
 
 @router.get("/")
-async def auth():
-    return "hello"
+async def auth(db: AsyncSession = Depends(get_db)):
+    try:
+        result = await db.execute(text("SELECT 1"))
+        value = result.scalar()
+        return {"res": value, "msg": "Database connection is working!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
