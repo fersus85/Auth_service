b8fc610eec34dde93d1da4c4ba76ab426d897479
maxim <maximsonis@gmail.com>
2024-12-14 12:01:45 +0300

    Add dockstring

--- src/services/helpers.py
+++ src/services/helpers.py
@@ -8,6 +8,13 @@ from services.utils import get_params_from_refresh_token
 
 
 class PermissionChecker:
+    """
+    Класс для проверки прав доступа пользователя.
+
+    Этот класс используется как зависимость в маршрутах FastAPI для проверки,
+    имеет ли текущий пользователь хотя бы одну из требуемых ролей. Если у пользователя
+    недостаточно прав, выбрасывается исключение HTTP 403 Forbidden.
+    """
     def __init__(self, required: Set[str]) -> None:
         self.required = required
 
@@ -16,6 +23,9 @@ class PermissionChecker:
         access: AccessJWT = Depends(get_params_from_refresh_token),
         role_service: RoleService = Depends(get_role_service),
     ) -> bool:
+        """
+        Проверяет, имеет ли пользователь хотя бы одну из требуемых ролей.
+        """
         if not any(True for role in access.roles if role in self.required):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
