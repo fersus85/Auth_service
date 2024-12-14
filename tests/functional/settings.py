6e060fa33b0d8253d89c8a49ecc9eb7a83a5ad5c
maxim <maximsonis@gmail.com>
2024-12-15 02:24:11 +0300

    Fix port for service test

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -5,7 +5,7 @@ class TestSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_ignore_empty=True, extra="ignore"
     )
-    SERVICE_URL: str = "http://localhost:8100"
+    SERVICE_URL: str = "http://localhost:8000"
     TEST_USER_LOGIN: str
     TEST_USER_PASSWORD: str
 
