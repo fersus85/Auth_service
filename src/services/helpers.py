1bb78108b33d6f4c69af5687962ec16d49f88cef
deriabin_85 <deriabin_85@mail.ru>
2024-12-25 11:12:22 +0300

    Добавил generate_secure_password для  yndx oauth

--- src/services/helpers.py
+++ src/services/helpers.py
@@ -1,10 +1,47 @@
+import secrets
+import string
 from typing import Set
 
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, Response, status
 
-from schemas.auth import AccessJWT
+from schemas.auth import AccessJWT, UserTokenResponse
 from services.role.role_service import RoleService, get_role_service
 from services.utils import get_params_from_refresh_token
+
+
+def set_tokens_in_cookies(
+    response: Response, tokens: UserTokenResponse
+) -> None:
+    """
+    Устанавливет токены в cookies
+    """
+    response.set_cookie(
+        key="access_token",
+        value=tokens.access_token,
+        httponly=True,
+        samesite="lax",
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=tokens.refresh_token,
+        httponly=True,
+        samesite="lax",
+    )
+
+
+def generate_secure_password(length=12):
+    """
+    Генерирует безопасный пароль заданной длины.
+
+    Параметры:
+    length (int): Длина генерируемого пароля. По умолчанию 12.
+
+    Возвращает:
+    str: Сгенерированный пароль, состоящий из букв, цифр и спец символов.
+    """
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(secrets.choice(characters) for _ in range(length))
+    return password
 
 
 class PermissionChecker:
