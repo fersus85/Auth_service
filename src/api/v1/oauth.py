69e55d3585f1937ea3c453c2b3a1cc75ebb54860
deriabin_85 <deriabin_85@mail.ru>
2024-12-27 13:17:47 +0300

    Добавил трассировку в обработчики yandex oauth

--- src/api/v1/oauth.py
+++ src/api/v1/oauth.py
@@ -2,7 +2,16 @@ import logging
 from typing import Annotated
 from urllib.parse import urlencode
 
-from fastapi import APIRouter, Depends, Header, Query, Response, status
+from fastapi import (
+    APIRouter,
+    Depends,
+    Header,
+    Query,
+    Request,
+    Response,
+    status,
+)
+from fastapi.responses import RedirectResponse
 
 from core.config import settings
 from schemas.yndx_oauth import UserInfoSchema
@@ -12,6 +21,7 @@ from services.helpers import (
     yndx_info_request,
     yndx_token_request,
 )
+from services.tracer import Tracer, get_tracer
 
 logger = logging.getLogger(__name__)
 
@@ -20,18 +30,23 @@ router = APIRouter(prefix="/oauth", tags=["OAuth 2.0"])
 
 @router.get(
     "/yndx_social_login",
-    status_code=status.HTTP_200_OK,
+    status_code=status.HTTP_302_FOUND,
     summary="OAuth 2.0 Yndx login",
     description="Redirects to Yandex OAuth 2.0 login page.",
 )
-async def yndx_social_login():
-    params = {
-        "response_type": "code",
-        "client_id": settings.yndx_oauth.YNDX_CLIENT_ID,
-    }
-    auth_url = f"{settings.yndx_oauth.YNDX_CODE_URL}?{urlencode(params)}"
-    logger.warning("url: %s", auth_url)
-    return {"url_for_auth": auth_url}
+async def yndx_social_login(
+    request: Request, tracer: Tracer = Depends(get_tracer)
+) -> RedirectResponse:
+    request_id = request.headers.get("X-Request-Id")
+    with tracer.start_span("Social login yndx") as span:
+        span.set_attribute("http.request_id", request_id)
+        params = {
+            "response_type": "code",
+            "client_id": settings.yndx_oauth.YNDX_CLIENT_ID,
+        }
+        auth_url = f"{settings.yndx_oauth.YNDX_CODE_URL}?{urlencode(params)}"
+        logger.warning("url: %s", auth_url)
+        return RedirectResponse(auth_url)
 
 
 @router.get(
@@ -43,6 +58,7 @@ async def yndx_social_login():
 )
 async def yndx_callback(
     response: Response,
+    request: Request,
     code: str = Query(
         ...,
         title="code",
@@ -50,15 +66,29 @@ async def yndx_callback(
     ),
     auth_service: AuthService = Depends(get_auth_service),
     user_agent: Annotated[str | None, Header()] = None,
+    tracer: Tracer = Depends(get_tracer),
 ):
-    resp_token_dict = await yndx_token_request(code)
-    logger.warning("Response YNDX_TOKEN_URL: %s", resp_token_dict)
+    request_id = request.headers.get("X-Request-Id")
 
-    resp_info_dict = await yndx_info_request(resp_token_dict)
-    logger.warning("Response YNDX_INFO_URL: %s", resp_info_dict)
+    with tracer.start_span("yndx_callback") as span:
+        span.set_attribute("http.request_id", request_id)
 
-    user_info = UserInfoSchema(**resp_info_dict)
-    user, tokens = await auth_service.login_user_yndx(user_info, user_agent)
+        with tracer.start_span("Request for yndx access token") as inner_span:
+            inner_span.set_attribute("http.request_id", request_id)
+            resp_token_dict = await yndx_token_request(code)
+            logger.warning("Response YNDX_TOKEN_URL: %s", resp_token_dict)
+
+        with tracer.start_span("Request for user info to yndx") as inner_span:
+            inner_span.set_attribute("http.request_id", request_id)
+            resp_info_dict = await yndx_info_request(resp_token_dict)
+            logger.warning("Response YNDX_INFO_URL: %s", resp_info_dict)
+
+        with tracer.start_span("Login user") as inner_span:
+            inner_span.set_attribute("http.request_id", request_id)
+            user_info = UserInfoSchema(**resp_info_dict)
+            user, tokens = await auth_service.login_user_yndx(
+                user_info, user_agent, request_id
+            )
 
     set_tokens_in_cookies(response, tokens)
 
